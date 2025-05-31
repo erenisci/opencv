debac8799c55886e7754d159aa806aeda287deda
Eren İşci <isci.eren@gmail.com>
2025-05-31 15:42:44 +0300

    basics

---
+++ 0-Basics/read.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+
+# Reading Photos
+# img = cv.imread('Resources/Photos/cat.jpg')
+# cv.imshow('Cat', img)
+# cv.waitKey(0)
+
+# Reading Videos
+# # capture = cv.VideoCapture(1)  # 1, 2 or 3.. is for video camera on computer
+# capture = cv.VideoCapture('Resources/Videos/dog.mp4')
+
+# while True:
+#     isTrue, frame = capture.read()
+#     cv.imshow('Video', frame)
+
+#     if (cv.waitKey(20) & 0xFF) == ord('d'):
+#         break
+
+# capture.release()
+# # cv.destroyWindow('Video')
+# cv.destroyAllWindows()
